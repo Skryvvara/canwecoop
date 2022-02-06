aeddd2c62085859a5638841941e2900331380987
Dimitri Kaiser <darkyne.gaming@outlook.de>
2022-02-06 14:13:27 +0100

    feat: added proper footer

--- src/components/pageFooter.tsx
+++ src/components/pageFooter.tsx
@@ -1,18 +1,22 @@
 import styles from 'styles/components/pageFooter.module.scss';
 import { FunctionComponent } from 'react';
+import Link from 'next/link';
 
 export const PageFooter: FunctionComponent = () => {
 
   return(
     <footer className={styles.mainFooter}>
-      <ul className={styles.linkList}>
-        <li>
-          <a className='appLink' href="https://github.com/xdarkyne">GitHub</a>
-        </li>
-        <li>
-          <a className='appLink' href="https://github.com/xdarkyne/canwecoop">Source</a>
-        </li>
-      </ul>
+      <div className={'container'}>
+        <ul className={styles.footerLinks}>
+          <li><a href='https://github.com/xdarkyne/canwecoop' target='_blank' rel='noreferrer'>Source</a></li>
+          <li><a href='https://github.com/xdarkyne/' target='_blank' rel='noreferrer'>My Github</a></li>
+          <li><Link href={'/about#data'}>Data usage</Link></li>
+          <li><a href='' target='_blank' rel='noreferrer'>Roadmap</a></li>
+          <li><Link href='/about'>About</Link></li>
+        </ul>
+
+        <p>Copyright &copy; 2022 <a href="https://darkyne.com">Darkyne</a></p>
+      </div>
     </footer>
   );
 };
