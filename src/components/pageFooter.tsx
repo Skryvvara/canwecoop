74c653d0203db5e97f59e71069d7d26e7608171f
xDarkyne <darkyne.gaming@outlook.de>
2022-02-09 20:44:49 +0100

    feat: added status and roadmap to footer

--- src/components/pageFooter.tsx
+++ src/components/pageFooter.tsx
@@ -11,11 +11,12 @@ export const PageFooter: FunctionComponent = () => {
           <li><a href='https://github.com/xdarkyne/canwecoop' target='_blank' rel='noreferrer'>Source</a></li>
           <li><a href='https://github.com/xdarkyne/' target='_blank' rel='noreferrer'>My Github</a></li>
           <li><Link href={'/about#data'}>Data usage</Link></li>
-          <li><a href='' target='_blank' rel='noreferrer'>Roadmap</a></li>
+          <li><a href='https://darkyne.notion.site/CanWeCoop-Roadmap-d8f1af1c09244994b9bf15556fcda284' target='_blank' rel='noreferrer'>Roadmap</a></li>
           <li><Link href='/about'>About</Link></li>
+          <li><a href='https://darkyne.betteruptime.com' target='_blank' rel='noreferrer'>Status</a></li>
         </ul>
 
-        <p>Copyright &copy; 2022 <a href="https://darkyne.com">Darkyne</a></p>
+        <p>Copyright &copy; 2022 <a href='https://darkyne.com'>Darkyne</a></p>
       </div>
     </footer>
   );
