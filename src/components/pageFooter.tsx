60480a3be7fdb31c167855e9ef4f891b9a2e08d2
xDarkyne <darkyne.gaming@outlook.de>
2022-02-12 12:16:35 +0100

    feat: added link to new roadmap

--- src/components/pageFooter.tsx
+++ src/components/pageFooter.tsx
@@ -8,10 +8,10 @@ export const PageFooter: FunctionComponent = () => {
     <footer className={styles.mainFooter}>
       <div className={'container'}>
         <ul className={styles.footerLinks}>
-          <li><a href='https://github.com/xdarkyne/canwecoop' target='_blank' rel='noreferrer'>Source</a></li>
-          <li><a href='https://github.com/xdarkyne/' target='_blank' rel='noreferrer'>My Github</a></li>
+          <li><a href='https://github.com/xDarkyne/canwecoop' target='_blank' rel='noreferrer'>Source</a></li>
+          <li><a href='https://github.com/xDarkyne/' target='_blank' rel='noreferrer'>My Github</a></li>
           <li><Link href={'/about#data'}>Data usage</Link></li>
-          <li><a href='https://darkyne.notion.site/CanWeCoop-Roadmap-d8f1af1c09244994b9bf15556fcda284' target='_blank' rel='noreferrer'>Roadmap</a></li>
+          <li><a href='https://github.com/users/xDarkyne/projects/3/views/5' target='_blank' rel='noreferrer'>Roadmap</a></li>
           <li><Link href='/about'>About</Link></li>
           <li><a href='https://darkyne.betteruptime.com' target='_blank' rel='noreferrer'>Status</a></li>
         </ul>
