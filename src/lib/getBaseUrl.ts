75d84e3e78c59bcecd5777543e16a6a246a2380e
xDarkyne <darkyne.gaming@outlook.de>
2022-03-20 20:34:53 +0100

    refactor: removed usage of deprecated symbol

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,12 +1,12 @@
 export function getBaseUrl() {
-  if (process.browser) {
+  if (typeof window === 'undefined') {
     return '';
   }
   // reference for vercel.com
   if (process.env.DOMAIN) {
     return `${process.env.DOMAIN}`;
   }
-    
+
   // assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
