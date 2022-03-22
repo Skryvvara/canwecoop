23950b9cc41284fc540054cbc0f32ba9fdc20dd2
xDarkyne <darkyne.gaming@outlook.de>
2022-03-22 08:08:13 +0100

    fix: added missing semicolon and removed comment

--- src/pages/api/auth/return.ts
+++ src/pages/api/auth/return.ts
@@ -2,17 +2,13 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import passport from 'lib/passport';
 import router from 'lib/router';
 
-/*interface IAuthReturnResponse extends NextApiResponse {
-	redirect: (path: string) => any;
-}*/
-
 const path = '/api/auth/return';
 
 export default router
 	.use(path, passport.authenticate('steam', { failureRedirect: '/' }))
 	.get(path, (req: NextApiRequest, res: NextApiResponse) => {
 		const route = req.cookies['origin'];
-		res.setHeader('Set-Cookie', `origin=${req.headers.referer}; Max-Age=-1`)
+		res.setHeader('Set-Cookie', `origin=${req.headers.referer}; Max-Age=-1`);
 		if (!route) {
 			res.redirect('/');
 			return;
