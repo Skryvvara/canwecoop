085c80e50e0280bbe4ed0e6108870aa753a69c0c
xDarkyne <darkyne.gaming@outlook.de>
2022-06-12 13:54:19 +0200

    feat: added steam avatars to next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   images: {
-    domains: [ 'steamcdn-a.akamaihd.net', 'cdn.akamai.steamstatic.com', 'darkyne.com' ],
+    domains: [ 'steamcdn-a.akamaihd.net', 'cdn.akamai.steamstatic.com', 'avatars.akamai.steamstatic.com', 'darkyne.com' ],
   },
   i18n: {
     locales: ['en'],
