9c42123ea34468c134575fd5dc8a72dfd84e15db
Skryvvara <darkyne.gaming@outlook.de>
2023-06-09 01:48:48 +0200

    feat: split css variables into their own file

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AuthContextProvider } from "@/context";
-import "@/styles/globals.scss";
+import "@/styles/_globals.scss";
 import NextAdapterPages from "next-query-params/pages";
 import { QueryParamProvider } from "use-query-params";
 import type { AppProps } from "next/app";
