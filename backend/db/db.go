ef01081c9a0bf9d162f171046253b5c34df43275
Skryvvara <darkyne.gaming@outlook.de>
2023-06-28 21:05:08 +0200

    feat: reworked roles

--- backend/db/db.go
+++ backend/db/db.go
@@ -4,8 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"log"
-	"net/http"
-	"strconv"
 
 	"github.com/skryvvara/canwecoop/config"
 	"github.com/skryvvara/canwecoop/db/models"
@@ -59,9 +57,13 @@ func Migrate() {
 	}
 
 	defaultRoles := []string{
-		config.APP.Steam.SyncRole,
-		"manage_roles",
-		"manage_users",
+		config.APP.Roles.Admin,
+		config.APP.Roles.SyncGames,
+		config.APP.Roles.ManageCategories,
+		config.APP.Roles.ManageGenres,
+		config.APP.Roles.ManageGames,
+		config.APP.Roles.ManageBadGames,
+		config.APP.Roles.ManageUsers,
 	}
 
 	for _, v := range defaultRoles {
@@ -82,49 +84,4 @@ func Migrate() {
 	}
 
 	log.Println("Successfully completed migrations!")
-}
-
-type Pagination struct {
-	Page            int `json:"page"`
-	Size            int `json:"size"`
-	DefaultPageSize int
-	MaxPageSize     int
-	MinPageSize     int
-}
-
-func GetPaginationFromRequestQuery(r *http.Request, defaultPageSize, maxPageSize, minPageSize int) Pagination {
-	query := r.URL.Query()
-
-	page, err := strconv.Atoi(query.Get("page"))
-	if err != nil || page == 0 {
-		page = 1
-	}
-	size, err := strconv.Atoi(query.Get("size"))
-	if err != nil || size == 0 {
-		size = defaultPageSize
-	} else if size > maxPageSize && maxPageSize != 0 {
-		size = maxPageSize
-	} else if size < minPageSize && minPageSize != 0 {
-		size = minPageSize
-	}
-
-	pagination := Pagination{
-		Page:            page,
-		Size:            size,
-		DefaultPageSize: defaultPageSize,
-		MaxPageSize:     maxPageSize,
-		MinPageSize:     minPageSize,
-	}
-
-	return pagination
-}
-
-func Paginate(pagination Pagination) func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		page := pagination.Page
-		pageSize := pagination.Size
-
-		offset := (page - 1) * pageSize
-		return db.Offset(offset).Limit(pageSize)
-	}
 }
